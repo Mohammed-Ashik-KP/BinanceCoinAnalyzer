fd9282ba18f5b2a6b2306cb50c75051f4ba0d4e1
Aashiq-Otp <Aashiqotp.as@gmail.com>
2020-07-18 14:42:34 +0530

    Updates

---
+++ precache-manifest.28487390a01ebc84ebaaf878ca24dbac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc13f58f6ce75e7b801712b4208d138f",
+    "url": "/BinanceCoinAnalyzer/index.html"
+  },
+  {
+    "revision": "5abe01b0d90c8b8cf025",
+    "url": "/BinanceCoinAnalyzer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "be7053a80e087fc1114d",
+    "url": "/BinanceCoinAnalyzer/static/css/main.2f29bb6b.chunk.css"
+  },
+  {
+    "revision": "5abe01b0d90c8b8cf025",
+    "url": "/BinanceCoinAnalyzer/static/js/2.0e62dd32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/BinanceCoinAnalyzer/static/js/2.0e62dd32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7053a80e087fc1114d",
+    "url": "/BinanceCoinAnalyzer/static/js/main.badb630a.chunk.js"
+  },
+  {
+    "revision": "82c2493f46596f086feb",
+    "url": "/BinanceCoinAnalyzer/static/js/runtime-main.157b4e77.js"
+  }
+]);
